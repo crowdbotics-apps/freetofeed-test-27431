b52982f2329d2ce37755903d824ef50f34dd0947
Crowdbotics <team@crowdbotics.com>
2021-05-26 15:23:42 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/freetofeed_test_27431/MainActivity.java
@@ -0,0 +1,15 @@
+package com.freetofeed_test_27431;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "freetofeed_test_27431";
+  }
+}
